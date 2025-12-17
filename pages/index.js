a5f2723f639f0117c1a7236a47dc7fcdb6ac0dd0
attentioErik <erik@attentio.no>
2025-12-17 10:37:35 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>BusinessBooster Stripe server ✅</h1>
+      <p>API is running.</p>
+    </div>
+  );
+}
